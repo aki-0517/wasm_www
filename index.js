82a707ff79239b21af9762cb1c8560c50681dc4f
aki-0517 <akki.pp.777@gmail.com>
2023-11-26 22:28:37 +0900

    edit html

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-import * as wasm from "wasm-game-of-life";
-
-wasm.greet();
+import { Universe } from "wasm-game-of-life";
+      const pre = document.getElementById("game-of-life-canvas");
+      const universe = Universe.new();
+      const renderLoop = () => {
+      pre.textContent = universe.render();
+      universe.tick();
+      requestAnimationFrame(renderLoop);
+};
